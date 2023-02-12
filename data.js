06e52cf7e291e578537abe7231c135ae1307040f
Khader Syed <khadersyed@users.noreply.github.com>
2023-02-12 13:46:41 -0500

    push data so far

---
+++ data.js
@@ -0,0 +1,17 @@
+// http://timeline.knightlab.com/docs/options.html
+var additionalOptions = {
+    start_at_end: true,
+    default_bg_color: {r:0, g:0, b:0},
+    timenav_height: 250
+};
+
+
+// http://timeline.knightlab.com/docs/json-format.html
+var timeline_json = $.getJson
+// two arguments: the id of the Timeline container (no '#')
+// and the JSON object or an instance of TL.TimelineConfig created from
+// a suitable JSON object
+window.timeline = new TL.Timeline('timeline-embed'
+, timeline_json
+// , sample_json
+);
